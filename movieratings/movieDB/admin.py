0e1a34303720ea943c32da05a94cc14e07d6c397
Karthik <karthik.kasala@gmail.com>
2015-10-07 00:23:25 -0400

    modified admin pages

--- movieratings/movieDB/admin.py
+++ movieratings/movieDB/admin.py
@@ -1,16 +1,17 @@
 from django.contrib import admin
+from .models import Rater, Movie, Rating
 
 
 class RaterAdmin(admin.ModelAdmin):
-    list_display = ['user', 'movies', 'ratings']
+    list_display = []
 
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ['title', 'average_rating', 'number of ratings']
+    list_display = []
 
 class RatingAdmin(admin.ModelAdmin):
-    list_display = ['movie', 'rating']
+    list_display = []
 
 # Register your models here.
-# admin.site.register(Rater, RaterAdmin)
-# admin.site.register(Movie, MovieAdmin)
-# admin.site.register(Rating, RatingAdmin)
+admin.site.register(Rater, RaterAdmin)
+admin.site.register(Movie, MovieAdmin)
+admin.site.register(Rating, RatingAdmin)
