582a81a6df026a0e1c73ade09ee5e0eb7076a67f
Karthik <karthik.kasala@gmail.com>
2015-10-07 00:30:23 -0400

    commented out lines

--- movieratings/movieDB/admin.py
+++ movieratings/movieDB/admin.py
@@ -4,13 +4,17 @@ from .models import Rater, Movie, Rating
 
 class RaterAdmin(admin.ModelAdmin):
     list_display = []
+    pass
+
 
 class MovieAdmin(admin.ModelAdmin):
     list_display = []
+    pass
+
 
 class RatingAdmin(admin.ModelAdmin):
     list_display = []
-
+    pass
 # Register your models here.
 admin.site.register(Rater, RaterAdmin)
 admin.site.register(Movie, MovieAdmin)
--- movieratings/movieDB/models.py
+++ movieratings/movieDB/models.py
@@ -3,14 +3,13 @@ from django.db import models
 # Create your models here.
 class Rater(models.Model):
     rater = models.IntegerField()
-    def __str__(self):              # __unicode__ on Python 2
-        return self.rater_text
+    #return self.rater_text
 class Movie(models.Model):
     movie = models.CharField(max_length=200)
 
-    def __str__(self):              # __unicode__ on Python 2
-        return self.movie_text
+    #def __str__(self):              # __unicode__ on Python 2
+        #return self.movie_text
 class Rating(models.Model):
     rating = models.IntegerField()
-    def __str__(self):              # __unicode__ on Python 2
-        return self.rating_text
+    #def __str__(self):              # __unicode__ on Python 2
+        #return self.rating_text
