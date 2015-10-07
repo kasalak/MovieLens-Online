c35b74ac02b595e30b38da16a293e751787e0662
Karthik <karthik.kasala@gmail.com>
2015-10-07 00:18:54 -0400

    added admin pages/base for methods

--- movieratings/movieDB/admin.py
+++ movieratings/movieDB/admin.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
 
 
-# class RaterAdmin(admin.ModelAdmin):
-#     list_display = ['user', 'movies', 'ratings']
-#
-# class MovieAdmin(admin.ModelAdmin):
-#     list_display = ['title', 'average_rating', 'number of ratings']
-#
-# class RatingAdmin(admin.ModelAdmin):
-#     list_display = ['movie', 'rating']
+class RaterAdmin(admin.ModelAdmin):
+    list_display = ['user', 'movies', 'ratings']
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['title', 'average_rating', 'number of ratings']
+
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ['movie', 'rating']
 
 # Register your models here.
 # admin.site.register(Rater, RaterAdmin)
--- movieratings/movieDB/models.py
+++ movieratings/movieDB/models.py
@@ -2,8 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Rater(models.Model):
-    pass
+    rater = models.IntegerField()
+    def __str__(self):              # __unicode__ on Python 2
+        return self.rater_text
 class Movie(models.Model):
-    pass
+    movie = models.CharField(max_length=200)
+
+    def __str__(self):              # __unicode__ on Python 2
+        return self.movie_text
 class Rating(models.Model):
-    pass
+    rating = models.IntegerField()
+    def __str__(self):              # __unicode__ on Python 2
+        return self.rating_text
