cf14f9a10e7f9482d770d5106d8f4b54cfc0f500
Karthik <karthik.kasala@gmail.com>
2015-10-06 23:25:05 -0400

    added models

---
+++ movieratings/movieDB/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Rater(model.Model):
+
+
+class Movie(models.Model):
+
+class Rating(models.Model):
+    rating = models.NumericField
+    
