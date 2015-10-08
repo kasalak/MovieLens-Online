23c536d2b69dcb54e944430c454d350fd79f396f
Karthik <karthik.kasala@gmail.com>
2015-10-08 09:42:28 -0400

    accidentally deleted fixtures for movies/ratings

--- movieratings/movieDB/models.py
+++ movieratings/movieDB/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # Create your models here.
 # id is automatic
 
-
 class Rater(models.Model):
 
     MALE = 'M'
@@ -39,34 +38,86 @@ class Movie(models.Model):
 
 class Rating(models.Model):
     stars = models.IntegerField()
-    user = models.ForeignKey(Rater)
+    rater = models.ForeignKey(Rater)
     movie = models.ForeignKey(Movie)
 
     def __str__(self):              # __unicode__ on Python 2
-        return '@user {} gives {} a {}'.format(self.user, self.movie, self.stars)
+        return '@rater {} gives {} a {}'.format(self.rater, self.movie, self.stars)
 
     def load_ml_data():
         import csv
         import json
 
-        with open('ml-1m/users.dat') as f:
-            reader = csv.Dictreader(f, fieldnames='UserID, Gender,Age, Occupation, Zip-code',
+        with open('ml1m/raters.dat') as f:
+            reader = csv.Dictreader(f, fieldnames='raterID, Gender,Age, Occupation, Zipcode',
                                     delimiter='::')
 
             for row in reader:
-                users = []
-                user = {
+                raters = []
+                rater = {
                     'fields': {'gender': row['gender'],
                                'age': row['Age'],
                                'occupation': row['Occupation'],
-                               'zipcode': row['Zip-code']
+                               'zipcode': row['Zipcode']
                     },
                     'model': 'movieDB.Rater',
-                    'pk': int(row['UserID'])
+                    'pk': int(row['raterID'])
                 }
-                users.append(user)
+                raters.append(rater)
 
-            with open('users.json', 'w') as f:
-                f.write(json.dumps(users))
+            with open('raters.json', 'w') as f:
+                f.write(json.dumps(raters))
 
-            print(json.dumps(users))
+            print(json.dumps(raters))
+def load_ml_movies():
+    import csv
+    import json
+
+    movies = []
+
+    with open('ml1m/movies.dat', encoding='Windows1252') as f:
+        reader = csv.DictReader([line.replace('::', '\t') for line in f],
+                                fieldnames='MovieID::Title::Genres'.split(
+                                    '::'),
+                                delimiter='\t')
+
+
+        for row in reader:
+            movie = {
+                'fields': {
+                    'title': row['Title'],
+                },
+                'model': 'movieDB.Movie',
+                'pk': row['MovieID']
+            }
+
+            movies.append(movie)
+
+    with open('movies.json' 'w') as f:
+        f.write(json.dumps(movies))
+
+
+def load_ml_ratings():
+    import csv
+    import json
+
+    ratings = []
+
+    with open('ml1m/ratings.dat') as f:
+        reader = csv.DictReader([line.replace('::', '\t') for line in f],
+                                fieldnames='UserID::MovieID::Rating::Timestamp'.split('::'),
+                                delimiter='\t')
+        for row in reader:
+            rating = {
+                'fields': {
+                    'rating': row['Rating'],
+                    'movie_id': row['MovieID'],
+                    'rater_id': row['UserID'],
+                },
+                'model': 'movieDB.Ratings',
+            }
+
+            ratings.append(rating)
+
+        with open('ratings.json', 'w') as f:
+            f.write(json.dumps(ratings))
--- movieratings/movieDB/views.py
+++ movieratings/movieDB/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def movie_detail(request, movie_id):
+    movie = Movie.object.get(pk=movie_id)
+    return render(request,
+                  'template_name.html',
+                  {'movie': movie})
