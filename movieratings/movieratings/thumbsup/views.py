ac8b1eb24124b2e66b4c51456a1cda86649cc01e
Karthik <karthik.kasala@gmail.com>
2015-10-12 18:01:43 -0400

    added star ratings

--- movieratings/movieratings/thumbsup/views.py
+++ movieratings/movieratings/thumbsup/views.py
@@ -5,9 +5,20 @@ from .models import Movie, Rater, Rating
 
 def movie_detail(request, movie_id):
     movie = Movie.objects.get(pk=movie_id)
-    # ratings = movie.ratings_set.all()
-    # user_ratings= []
-    
     return render(request,
                   'thumbsup/movie_detail.html',
                   {'movie': movie})
+
+
+def rater_detail(request, rater_id):
+    rater = Rater.objects.get(pk=rater_id)
+    movie_ratings = []
+    for rating in rater.rating_set.all():
+        movie_ratings.append({
+            'movie': rating.movie,
+            'stars': '\u2605' * rating.stars,
+        })
+    return render(request,
+                  'thumbsup/rater_detail.html',
+                  {'rater': rater,
+                   'movie_ratings': movie_ratings})
