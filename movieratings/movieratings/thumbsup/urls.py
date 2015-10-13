3fb97c6edf63913e0c149e12420cbd982dfba38f
Karthik <karthik.kasala@gmail.com>
2015-10-12 22:48:01 -0400

    got a top20 page

--- movieratings/movieratings/thumbsup/urls.py
+++ movieratings/movieratings/thumbsup/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
+    url(r'$', views.top_movies, name='top_movies'),
     url(r'movie/(?P<movie_id>\d+)$', views.movie_detail, name='movie_detail'),
     url(r'rater/(?P<rater_id>\d+)$', views.rater_detail, name='rater_detail'),
     # url(r'rating/(?P<rating_id>\d+)$', views.rating_detail),
--- movieratings/movieratings/thumbsup/views.py
+++ movieratings/movieratings/thumbsup/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models import Avg, Count
 from .models import Movie, Rater, Rating
 # Create your views here.
 
@@ -22,3 +23,20 @@ def rater_detail(request, rater_id):
                   'thumbsup/rater_detail.html',
                   {'rater': rater,
                    'movie_ratings': movie_ratings})
+
+
+def top_movies(request):
+    # movie_list = []
+    # for movie in Movie.objects.all():
+    #     if type(movie.average_rating()) == float:
+    #         movie_list.append(movie)
+
+    popular_movies = Movie.objects.annotate(num_ratings=Count('rating')) \
+                                  .filter(num_ratings__gte=50)
+
+    movies = popular_movies.annotate(Avg('rating__stars')) \
+                           .order_by('-rating__stars__avg')[:20]
+
+    return render(request,
+                  'thumbsup/top_movies.html',
+                  {'movies': movies})
